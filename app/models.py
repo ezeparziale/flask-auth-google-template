018e436b242203a28e270b0247d0875d7771d6b3
Ezequiel Parziale <eparziale@gmail.com>
2023-03-30 19:23:26 -0300

    :card_file_box: update: upgrade model to sqlalchemy 2

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from flask import redirect, url_for
 from flask_login import UserMixin
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 
@@ -19,17 +22,18 @@ def unauthorized():
 
 class User(db.Model, UserMixin):  # type: ignore  # noqa
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String(40), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    created_at = Column(
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=text("CURRENT_TIMESTAMP"),
         nullable=False,
     )
 
     def __repr__(self) -> str:
-        return f"{self.username} : {self.email} : {self.created_at}"
+        return f"User(id={self.id}, username={self.username}, email={self.email}, created_at={self.created_at})"
 
     def save(self):
         db.session.add(self)
