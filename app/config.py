ef01a1405f58946f9b25881d416252a3b426cacc
Ezequiel Parziale <eparziale@gmail.com>
2025-09-16 20:45:42 -0300

    :zap: chore: improve comment sections

--- app/config.py
+++ app/config.py
@@ -5,15 +5,13 @@ class Settings(BaseSettings):
     # Flask
     SECRET_KEY: str
 
-    # Postgres
+    # Database
     SQLALCHEMY_DATABASE_URI: str
     SQLALCHEMY_TRACK_MODIFICATIONS: str
 
-    # Google OAuth2
+    # Auth Google
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
-
-    # Google OAuth2 settings
     GOOGLE_SCOPES: list = ["openid", "email", "profile"]
     GOOGLE_REDIRECT: str = "https://127.0.0.1:5000/authorize"
     GOOGLE_AUTHORIZATION_BASE_URL: str = "https://accounts.google.com/o/oauth2/auth"
