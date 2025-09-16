6a7982e510600cc260925b8f6c56f07eb26bb2ab
Ezequiel Parziale <eparziale@gmail.com>
2025-09-16 19:42:10 -0300

    :zap: chore: add section comment

--- app/config.py
+++ app/config.py
@@ -2,12 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # Flask
     SECRET_KEY: str
+
+    # Postgres
     SQLALCHEMY_DATABASE_URI: str
     SQLALCHEMY_TRACK_MODIFICATIONS: str
+
+    # Google OAuth2
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
 
+    # Google OAuth2 settings
     GOOGLE_SCOPES: list = ["openid", "email", "profile"]
     GOOGLE_REDIRECT: str = "https://127.0.0.1:5000/authorize"
     GOOGLE_AUTHORIZATION_BASE_URL: str = "https://accounts.google.com/o/oauth2/auth"
