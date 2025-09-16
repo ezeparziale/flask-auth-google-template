e42dd5983df982262bff3db319c40d3ac70c44c7
Ezequiel Parziale <eparziale@gmail.com>
2025-09-16 19:32:48 -0300

    :truck: chore: relocate alembic to app folder for better structure

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from alembic import context
 from app.config import settings
 
 # this is the Alembic Config object, which provides
--- app/alembic/versions/962230cd7245_init_db.py
+++ app/alembic/versions/962230cd7245_init_db.py
@@ -7,7 +7,6 @@ Create Date: 2022-08-18 20:14:15.605744
 """
 
 import sqlalchemy as sa
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
