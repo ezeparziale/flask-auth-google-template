d73800d45713410fae1aa3e806a209ab78079ab6
Ezequiel Parziale <eparziale@gmail.com>
2025-09-16 19:30:17 -0300

    :card_file_box: refactor: upgrade alembic config

--- alembic/env.py
+++ alembic/env.py
@@ -61,7 +61,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),  # type: ignore  # noqa
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
